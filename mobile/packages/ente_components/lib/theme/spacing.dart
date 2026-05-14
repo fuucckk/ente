2d9121bcaf095eff7addba4e1878911c23780c2e
ashilkn <ashilkn99@gmail.com>
2026-05-14 14:55:53 +0530

    Update spacing to match figma design system

--- mobile/packages/ente_components/lib/theme/spacing.dart
+++ mobile/packages/ente_components/lib/theme/spacing.dart
@@ -4,12 +4,9 @@
 class Spacing {
   const Spacing._();
 
-  static const double xxs = 2;
   static const double xs = 4;
   static const double sm = 8;
   static const double md = 12;
   static const double lg = 16;
-  static const double xl = 24;
-  static const double xxl = 32;
-  static const double xxxl = 40;
+  static const double xl = 20;
 }
